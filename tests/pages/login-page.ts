9ffe225f35430aad3b6a834acd4f56abb21cf8dd
AndrewKhv <a.d.khramchenkov@gmail.com>
2025-11-17 17:33:54 +0100

    feat: TL-17 fixed type error

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -1,4 +1,5 @@
-import { Locator, Page } from '@playwright/test'
+import { Page } from '@playwright/test'
+import type { Locator } from '@playwright/test'
 import { OrderPage } from './order-page'
 import { SERVICE_URL } from '../../config/env-data'
 
--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,4 +1,5 @@
-import { expect, Locator, Page } from '@playwright/test'
+import { expect, Page } from '@playwright/test'
+import type { Locator } from '@playwright/test'
 
 export class OrderPage {
   readonly page: Page
